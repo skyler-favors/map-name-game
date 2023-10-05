3d5df4d2a1a70b9626f87ef65c4e14cfea5e5c36
skylerfavors <sfavors@geo-jobe.com>
2023-10-05 10:55:57 -0500

    debugging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use axum::Router;
 use dotenv::dotenv;
 use std::env;
-use std::fs;
 use tower_http::{
     services::ServeDir,
     trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
@@ -13,22 +12,19 @@ async fn main() {
     dotenv().ok();
     let port = env::var("PORT").unwrap();
 
-    let paths = fs::read_dir("./").unwrap();
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
-    }
-
     tracing_subscriber::fmt()
         .with_target(false)
         .compact()
         .init();
 
     // build our application with a single route
-    let app = Router::new().nest_service("/", ServeDir::new("www")).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
-            .on_response(DefaultOnResponse::new().level(Level::INFO)),
-    );
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("assets"))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
+        );
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&format!("0.0.0.0:{}", port).parse().unwrap())
