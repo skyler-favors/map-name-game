4ecbacf6d929fb5ef1e828e4653b2dc2ce164a0b
skylerfavors <sfavors@geo-jobe.com>
2023-10-05 10:22:03 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+
+use axum::Router;
+use dotenv::dotenv;
+use tower_http::{
+    services::ServeDir,
+    trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
+};
+use tracing::Level;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let port = env::var("PORT").unwrap();
+
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    // build our application with a single route
+    let app = Router::new().nest_service("/", ServeDir::new("www")).layer(
+        TraceLayer::new_for_http()
+            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(DefaultOnResponse::new().level(Level::INFO)),
+    );
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&format!("0.0.0.0:{}", port).parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
